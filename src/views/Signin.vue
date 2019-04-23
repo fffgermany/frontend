f378aed70065670e55af0d2e2ca2e5ab0145b2f8
Marcel Quasnica <info@marcelquasnica.de>
2019-04-23 13:21:35 +0200

    change to scss

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -63,53 +63,53 @@ export default {
 </script>
 
 
-<style lang="css">
+<style lang="scss">
 
 .fff-signin{
-width: 75%;
-margin: auto;
-margin-top: 3cm;
-border: 2px solid #1B7340;
-padding: 15px 15px 15px 15px;
-border-radius: 4px;
-box-sizing: border-box;
--webkit-appearance: none;
-}
+  width: 75%;
+  margin: auto;
+  margin-top: 3cm;
+  border: 2px solid #1B7340;
+  padding: 15px 15px 15px 15px;
+  border-radius: 4px;
+  box-sizing: border-box;
+  -webkit-appearance: none;
 
-.fff-signin button{
-box-sizing: border-box;
-background: #1B7340;
-color: white !important;
-width: 100%;
-padding: 10px;
-margin-top: 5px;
-border-radius: 4px;
-text-transform: capitalize;
-border: none;
-outline: none;
-}
+  button {
+    box-sizing: border-box;
+    background: #1B7340;
+    color: white !important;
+    width: 100%;
+    padding: 10px;
+    margin-top: 5px;
+    border-radius: 4px;
+    text-transform: capitalize;
+    border: none;
+    outline: none;
 
-.fff-signin button:hover{
-background: #1DA64A;
-border: none;
-}
+    &:hover{
+      background: #1DA64A;
+      border: none;
+    }
+  }
 
-.fff-signin input{
-box-sizing: border-box;
-border: 2px solid #1B7340;
-border-radius: 4px;
-margin-top: 5px;
-outline: none;
-width: 100%;
-padding: 10px 0px 10px 10px;
-}
+  input{
+    box-sizing: border-box;
+    border: 2px solid #1B7340;
+    border-radius: 4px;
+    margin-top: 5px;
+    outline: none;
+    width: 100%;
+    padding: 10px 0px 10px 10px;
 
-.fff-signin__input:focus{
-background: rgba(27,115,64,0.5);
-}
+    &:focus{
+      background: rgba(27,115,64,0.5);
+    }
+  }
 
 @media only screen and (min-width: 768px) {
- .fff-signin{width: 35%}
+  width: 35%
  }
+}
 
 </style>
