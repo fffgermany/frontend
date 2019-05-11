64ed04caead9edea4fb8eb0e5a8f4da3030460cc
Lucas Hild <contact@lucas-hild.de>
2019-05-11 10:39:31 +0200

    Update input style on signin and signup

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,71 +1,49 @@
 <template>
-  <div class="fff-signin main-container">
-    <form v-if="!signinSuccess">
-      <div>
-        <input class="fff-signin__input fff-input"
-              v-model="model.email"
-              name="email"
-              type="email"
-              placeholder="Email"/>
-      </div>
-      <div>
-        <input class="fff-signin__input fff-input"
-              v-model="model.password"
-              name="password"
-              type="password"
-              placeholder="Password"/>
-      </div>
-      <div>
-        <button @click="submit">sign in</button>
-      </div>
-    </form>
-  </div>
+    <div class="fff-signin main-container">
+        <form v-if="!signinSuccess">
+            <div class="input-wrapper">
+                <label>E-Mail-Adresse</label>
+                <input v-model="model.email" type="email" placeholder="E-Mail-Adresse"/>
+            </div>
+
+            <div class="input-wrapper">
+                <label>Passwort</label>
+                <input v-model="model.password" type="password" placeholder="Passwort"/>
+            </div>
+
+            <div>
+                <button @click="submit">Anmelden</button>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
 
 export default {
-  name: 'signin',
-  data() {
-    return {
-      model: {
-        email: null,
-        password: null,
-      },
-      signinSuccess: false,
-    };
-  },
-  computed: {
-    localgroups() {
-      return this.$store.getters['localgroups/getItems'];
-    },
-  },
-  components: {
+    name: 'signin',
 
-  },
-  methods: {
-    submit(e) {
-      e.preventDefault();
-
-      this.$store.dispatch('signin', this.model)
-        .then(() => this.$router.push('demos'))
-        .catch((err) => {
-          console.log(err);
-        });
+    data() {
+        return {
+            model: {
+                email: null,
+                password: null,
+            },
+            signinSuccess: false,
+        }
     },
-  },
-};
+
+    methods: {
+        async submit(e) {
+            e.preventDefault();
+
+            await this.$store.dispatch('signin', this.model)
+            this.$router.push('home')
+        },
+    },
+}
 </script>
 
 
 <style scoped>
-.fff-signin {
-  width: 75%;
-  margin: auto;
-  margin-top: 3cm;
-  border: 2px solid #1B7340;
-  padding: 15px 15px 15px 15px;
-  border-radius: 4px;
-  -webkit-appearance: none;
-}
 </style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,130 +1,105 @@
 <template>
-  <div class="fff-signup main-container">
-    <form v-if="!signupSuccess">
-      <div>
-        <input class="fff-signup__input fff-input"
-              v-model="model.email"
-              name="email"
-              type="email"
-              placeholder="Email"/>
-        <span class="fff-input__error">{{error.email}}</span>
-      </div>
-      <div>
-        <input class="fff-signup__input fff-input"
-              v-model="model.name"
-              name="name"
-              type="text"
-              placeholder="Name"/>
-        <span class="fff-input__error">{{error.name}}</span>
-      </div>
-      <div>
-        <input class="fff-signup__input fff-input"
-              v-model="model.description"
-              name="description"
-              type="text"
-              placeholder="Beschreibung"/>
-        <span class="fff-input__error">{{error.description}}</span>
-      </div>
-      <div>
-        <select class="fff-signup__input fff-input"
-              v-model="model.ortsgruppe_id"
-              name="ortsgruppe">
-              <option value="null" disabled hidden>Ortsgruppe</option>
-              <option v-for="localgroup in localgroups" v-bind:value="localgroup.id" v-bind:key="localgroup.id">
-            {{ localgroup.name }}
-          </option>
-        </select>
-        <span class="fff-input__error"></span>
-      </div>
-      <div>
-        <input class="fff-signup__input fff-input"
-              v-model="model.password"
-              name="password"
-              type="password"
-              placeholder="Password"/>
-        <span class="fff-input__error">{{error.password}}</span>
-      </div>
-      <div>
-        <input class="fff-signup__input fff-input"
-              v-model="model.password"
-              name="password_repeat"
-              type="password"
-              placeholder="Password wiederholen"/>
-      </div>
-      <div>
-        <input class="fff-signup__input fff-input"
-              v-model="model.thesecret"
-              name="thesecret"
-              type="text"
-              placeholder="Secret"/>
-        <span class="fff-input__error">{{error.thesecret}}</span>
-      </div>
-      <div>
-        <button @click="submit">registrieren</button>
-      </div>
-    </form>
-    <div v-if="signupSuccess">
-      Wir haben dir eine Email geschickt um deine Email-Adresse zu verifizieren. Wenn deine Email-Adresse verifiziert ist schalten wir dein Konto frei.
+    <div class="fff-signup main-container">
+        <form v-if="!signupSuccess">
+            <div class="input-wrapper">
+                <label>Name</label>
+                <input v-model="model.name" placeholder="Name"/>
+                <span class="fff-input__error">{{error.name}}</span>
+            </div>
+
+            <div class="input-wrapper">
+                <label>E-Mail-Adresse</label>
+                <input v-model="model.email" type="email" placeholder="E-Mail-Adresse"/>
+                <span class="fff-input__error">{{error.email}}</span>
+            </div>
+
+            <div class="input-wrapper">
+                <label>Beschreibung</label>
+                <input v-model="model.description" placeholder="Beschreibung"/>
+                <span class="fff-input__error">{{error.description}}</span>
+            </div>
+
+            <div class="input-wrapper">
+                <label>Ortsgruppe</label>
+                <select class="fff-signup__input fff-input"
+                    v-model="model.ortsgruppe_id"
+                    name="ortsgruppe">
+                    <option value="null" disabled hidden>Ortsgruppe</option>
+                        <option v-for="localgroup in localgroups" v-bind:value="localgroup.id" v-bind:key="localgroup.id">
+                        {{ localgroup.name }}
+                    </option>
+                </select>
+                <span class="fff-input__error">{{error.ortsgruppe_id}}</span>
+            </div>
+
+            <div class="input-wrapper">
+                <label>Passwort</label>
+                <input v-model="model.password" placeholder="Neues Passwort festlegen" type="password"/>
+                <span class="fff-input__error">{{error.password}}</span>
+            </div>
+
+            <div class="input-wrapper">
+                <label>Passwort wiederholen</label>
+                <input v-model="model.password" placeholder="Passwort wiederholen" type="password"/>
+                <span class="fff-input__error">{{error.password}}</span>
+            </div>
+
+            <div class="input-wrapper">
+                <label>Secret</label>
+                <input v-model="model.thesecret" placeholder="Secret"/>
+                <span class="fff-input__error">{{error.thesecret}}</span>
+            </div>
+
+            <div>
+                <button @click="submit">registrieren</button>
+            </div>
+        </form>
+
+        <div v-if="signupSuccess">
+            Wir haben dir eine Email geschickt, um deine Email-Adresse zu verifizieren.
+            Wenn deine Email-Adresse verifiziert ist, schalten wir dein Konto frei.
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 
 export default {
-  name: 'signup',
-  data() {
-    return {
-      model: {
-        email: null,
-        name: null,
-        ortsgruppe_id: null,
-        password: null,
-        thesecret: null,
-      },
-      signupSuccess: false,
-      error: {},
-    };
-  },
-  computed: {
-    localgroups() {
-      return this.$store.getters['localgroups/getItems'];
+    name: 'signup',
+    data() {
+        return {
+            model: {
+                email: null,
+                name: null,
+                ortsgruppe_id: null,
+                password: null,
+                thesecret: null,
+            },
+            signupSuccess: false,
+            error: {},
+        }
     },
-  },
-  components: {
 
-  },
-  methods: {
-    submit(e) {
-      e.preventDefault();
-
-      this.$store.dispatch('signup', this.model)
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((err) => {
-          console.log(err);
-          this.error = err.response.data;
-        });
+    computed: {
+        localgroups() {
+            return this.$store.getters['localgroups/getItems'];
+        },
     },
-  },
-  beforeCreate() {
-    this.$store.dispatch('localgroups/getList');
-  },
-};
+
+    methods: {
+        async submit(event) {
+            event.preventDefault();
+
+            let response = await this.$store.dispatch('signup', this.model)
+            // this.error = err.response;
+        },
+    },
+
+    beforeCreate() {
+        this.$store.dispatch('localgroups/getList');
+    },
+}
 </script>
 
 <style scoped>
-.fff-signup{
-    width: 75%;
-    margin: auto;
-    margin-top: 3cm;
-    border: 2px solid #1B7340;
-    padding: 15px 15px 15px 15px;
-    border-radius: 4px;
-    box-sizing: border-box;
-    -webkit-appearance: none;
-}
-
-
 </style>
