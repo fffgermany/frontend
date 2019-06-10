3947aa7dd2c7480861db7aa976e6a7cd6f3dbaa1
Lucas Hild <contact@lucas-hild.de>
2019-06-10 17:43:25 +0200

    Update style of demo list

---
+++ src/components/DemoPreview.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="fff-demo-preview" :class="{ open }">
+        <router-link class="left" :to="{ name: 'demoView', params: { id: demo.id } }">
+            <p class="place">{{ demo.ort }}</p>
+
+            <div v-if="open">
+                <p>Startzeit: {{ demo.zeit }}</p>
+                <p>Ortsgruppe: {{ demo.ortsgruppeName }}</p>
+                <p>Teilnehmerzahl: {{ demo.teilnehmerzahl }}</p>
+            </div>
+        </router-link>
+
+        <div class="right" @click="toggle">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'fff-demo-preview',
+
+    props: ['demo'],
+
+    data() {
+        return {
+            open: false
+        }
+    },
+
+    methods: {
+        toggle() {
+            this.open = !this.open
+        }
+    }
+}
+</script>
+
+<style scoped>
+.fff-demo-preview {
+    display: block;
+    background: rgb(250, 250, 250);
+    margin-bottom: 10px;
+
+    display: grid;
+    grid-template-columns: 1fr auto;
+}
+
+.fff-demo-preview:hover {
+    background: rgb(240, 240, 240);
+}
+
+.fff-demo-preview:hover p {
+    color: var(--primary-color);
+}
+
+.left {
+    padding: 10px;
+}
+
+.fff-demo-preview.open .place {
+    font-weight: bold;
+}
+
+.right {
+    display: grid;
+    place-items: center;
+    padding: 0 10px;
+}
+
+.right svg {
+    transition: transform 0.25s ease-in-out;
+    cursor: pointer;
+}
+
+.right svg:hover {
+    fill: var(--primary-color);
+}
+
+.open .right svg {
+    transform: rotate(90deg)
+}
+</style>
--- src/views/DemoList.vue
+++ src/views/DemoList.vue
@@ -10,22 +10,13 @@
             </div>
         </div>
 
-        <vue-virtual-table
-            :enableExport="true"
-            :height="windowHeight"
-            :config="tableConfig"
-            :data="demos">
-            <template slot-scope="scope" slot="actionCommon">
-                <!-- ToDo add btns for  demo admin shortcuts (delete, edit, copy) -->
-                <button @click="goToDemo(scope.row.id)">Details anzeigen</button>
-            </template>
-        </vue-virtual-table>
+
+        <DemoPreview v-for="demo in demos" :demo="demo" :key="demo.id"/>
     </div>
 </template>
 
 <script>
-
-import VueVirtualTable from 'vue-virtual-table';
+import DemoPreview from '@/components/DemoPreview'
 
 export default {
     name: 'demoList',
@@ -33,32 +24,16 @@ export default {
     data() {
         return {
             filterByAdminID: false,
-            tableConfig: [
-                {
-                    prop: 'ort', name: 'Treffpunkt', sortable: true, searchable: true,
-                },
-                {
-                    prop: 'ortsgruppeName', name: 'Ortsgruppe', sortable: true, searchable: true,
-                },
-                {
-                    prop: 'zeit', name: 'Startzeit', sortable: true, searchable: true,
-                },
-                {
-                    prop: 'teilnehmerzahl', name: 'Teilnehmerzahl', sortable: true, searchable: true,
-                },
-                {
-                    prop: '_action', name: ' ', actionName: 'actionCommon'
-                },
-            ],
-        };
+        }
     },
 
-    components: { VueVirtualTable },
+    components: { DemoPreview },
 
     computed: {
         localgroups() {
             return this.$store.getters['localgroups/getItems'];
         },
+
         demos() {
             const demos = this.$store.getters['demos/getItems'];
 
@@ -74,12 +49,9 @@ export default {
 
         user() {
             return this.$store.getters['getUser']();
-        },
-
-        windowHeight() {
-            return window.innerHeight * 0.7;
-        },
+        }
     },
+
     methods: {
         addNewDemo() {
             this.$router.push({ name: 'demoView', params: { id: 'new' } });
@@ -89,6 +61,7 @@ export default {
             this.$router.push({ name: 'demoView', params: { id: id } });
         },
     },
+
     beforeCreate() {
         this.$store.dispatch('demos/getList');
         this.$store.dispatch('localgroups/getList');
