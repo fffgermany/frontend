bbda5fdb7203987dcad22098e58749036f5664d9
Lucas Hild <contact@lucas-hild.de>
2019-06-10 17:02:01 +0200

    Add search bar to list of localgroups

--- src/views/GroupList.vue
+++ src/views/GroupList.vue
@@ -2,21 +2,36 @@
     <div class="fff-group-list main-container">
         <h2>Ortgruppen</h2>
 
-        <GroupPreview v-for="localgroup in localgroups" :localgroup="localgroup" :key="localgroup.id"/>
+        <SearchBar v-model="search"/>
+        <GroupPreview v-for="localgroup in filteredLocalgroups" :localgroup="localgroup" :key="localgroup.id"/>
+        
+        <p v-if="filteredLocalgroups.length == 0">Keine Ortsgruppe gefunden!</p>
     </div>
 </template>
 
 <script>
 import GroupPreview from '@/components/GroupPreview'
+import SearchBar from '@/components/SearchBar'
 
 export default {
     name: 'fff-group-list',
 
-    components: { GroupPreview },
+    components: { GroupPreview, SearchBar },
+
+    data() {
+        return {
+            search: ''
+        }
+    },
 
     computed: {
         localgroups() {
             return this.$store.getters['localgroups/getItems'];
+        },
+
+        filteredLocalgroups() {
+            let search = this.search.toLowerCase();
+            return this.localgroups.filter(localgroup => localgroup.name.toLowerCase().includes(search))
         }
     },
 
